d021f0592cc4d23b1a3f3e9537c978d25f408b95
mauriciogamboa <mauricio@heylaika.com>
2022-05-11 17:36:03 -0600

    Added # 2.

--- app/src/main/java/com/example/prueba_concepto/MainActivity.java
+++ app/src/main/java/com/example/prueba_concepto/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //prueba de concepto.
+        //prueba de concepto # 2
     }
 }
